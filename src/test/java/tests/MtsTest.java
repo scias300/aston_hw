f3b0903c72e85d1233e0aec9538985437c7eda91
Alexander Pashkov <scias300@gmail.com>
2026-07-03 19:36:53 +0300

    task done

--- src/test/java/tests/MtsTest.java
+++ src/test/java/tests/MtsTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.qameta.allure.Allure;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
@@ -17,80 +18,102 @@ public class MtsTest {
     void mtsPayTest() {
         WebDriver driver = new ChromeDriver();
         MtsPage page = new MtsPage(driver);
-        page.open();
-        page.acceptCookies();
-
-        String payHeaderExpected = "ОНЛАЙН ПОПОЛНЕНИЕ\nБЕЗ КОМИССИИ";
-        String payHeaderActual = page.getPayHeader();
-        Assertions.assertEquals(payHeaderExpected, payHeaderActual);
-
-        List<String> expectedLogos = List.of(
-                "Visa",
-                "Verified By Visa",
-                "MasterCard",
-                "MasterCard Secure Code",
-                "Белкарт"
-        );
-        List<String> actualLogos = page.getPartnersLogos();
-        Assertions.assertEquals(expectedLogos, actualLogos);
-
-        page.openDetailsLink();
-        assertTrue(driver.getCurrentUrl().contains("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
-        driver.navigate().back();
-
-        List<String> expectedSelectOptions = List.of(
-                "Услуги связи",
-                "Домашний интернет",
-                "Рассрочка",
-                "Задолженность"
-        );
-        List<String> actualSelectOptions = page.getSelectOptions();
-        assertTrue(actualSelectOptions.containsAll(expectedSelectOptions));
-
-
-        Assertions.assertEquals("Услуги связи", page.getCurrentSelectText());
         String phoneNumber = "297777777";
         String sum = "11.1";
-        page.enterPhoneNumber(phoneNumber);
-        page.enterSum(sum);
-        page.clickPay();
+        Allure.step("Открытие страницы и принятие cookies", () -> {
+            page.open();
+            page.acceptCookies();
+        });
 
-        WebElement paymentWidget = page.getPaymentWidget();
-        assertTrue(paymentWidget.isDisplayed());
-        driver.switchTo().frame(paymentWidget);
 
-        Assertions.assertEquals(
-                Double.parseDouble(sum),
-                Double.parseDouble(page.getPayDescriptionCost())
-        );
-        Assertions.assertEquals(
-                Double.parseDouble(sum),
-                Double.parseDouble(page.getButtonSum())
-        );
-        Assertions.assertEquals(
-                Double.parseDouble(sum),
-                Double.parseDouble(page.getAgreementSum())
-        );
+        Allure.step("Проверка названия блока", () -> {
+            String payHeaderExpected = "ОНЛАЙН ПОПОЛНЕНИЕ\nБЕЗ КОМИССИИ";
+            String payHeaderActual = page.getPayHeader();
+            Assertions.assertEquals(payHeaderExpected, payHeaderActual);
+        });
 
-        String expectedPhoneNumber = "375" + phoneNumber;
-        String actualPhoneNumber = page.getPhoneNumber();
-        Assertions.assertEquals(expectedPhoneNumber, actualPhoneNumber);
+        Allure.step("Проверка логотипов платежных систем", () -> {
+            List<String> expectedLogos = List.of(
+                    "Visa",
+                    "Verified By Visa",
+                    "MasterCard",
+                    "MasterCard Secure Code",
+                    "Белкарт"
+            );
+            List<String> actualLogos = page.getPartnersLogos();
+            Assertions.assertEquals(expectedLogos, actualLogos);
+        });
 
-        List<String> expectedCardPlaceholders = List.of(
-                "Номер карты",
-                "Срок действия",
-                "CVC",
-                "Имя и фамилия на карте"
-        );
-        List<String> actualCardPlaceholders = page.getCardPlaceholders();
-        assertEquals(expectedCardPlaceholders, actualCardPlaceholders);
+        Allure.step("Проверка ссылки 'Подробнее о сервисе'", () -> {
+            page.openDetailsLink();
+            assertTrue(driver.getCurrentUrl().contains("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
+            driver.navigate().back();
+        });
 
-        List<WebElement> cardBrandsImg = page.getCardBrandsImg();
-        assertFalse(cardBrandsImg.isEmpty());
 
-        for (WebElement image : cardBrandsImg) {
-            assertFalse(image.getAttribute("src").isBlank());
-        }
+        Allure.step("Проверка выпадающего меню с типами услуг", () -> {
+            List<String> expectedSelectOptions = List.of(
+                    "Услуги связи",
+                    "Домашний интернет",
+                    "Рассрочка",
+                    "Задолженность"
+            );
+            List<String> actualSelectOptions = page.getSelectOptions();
+            assertTrue(actualSelectOptions.containsAll(expectedSelectOptions));
+        });
+
+
+        Allure.step("Проверка заполнения полей оплаты и кнопки 'Продолжить'", () -> {
+            Assertions.assertEquals("Услуги связи", page.getCurrentSelectText());
+            page.enterPhoneNumber(phoneNumber);
+            page.enterSum(sum);
+            page.clickPay();
+        });
+
+        Allure.step("Проверка открытия нового окна и отображения суммы", () -> {
+            WebElement paymentWidget = page.getPaymentWidget();
+            assertTrue(paymentWidget.isDisplayed());
+            driver.switchTo().frame(paymentWidget);
+
+            Assertions.assertEquals(
+                    Double.parseDouble(sum),
+                    Double.parseDouble(page.getPayDescriptionCost())
+            );
+            Assertions.assertEquals(
+                    Double.parseDouble(sum),
+                    Double.parseDouble(page.getButtonSum())
+            );
+            Assertions.assertEquals(
+                    Double.parseDouble(sum),
+                    Double.parseDouble(page.getAgreementSum())
+            );
+        });
+
+        Allure.step("Проверка отображения номера телефона", () -> {
+            String expectedPhoneNumber = "375" + phoneNumber;
+            String actualPhoneNumber = page.getPhoneNumber();
+            Assertions.assertEquals(expectedPhoneNumber, actualPhoneNumber);
+        });
+
+        Allure.step("Проверка полей для ввода реквизитов карты", () -> {
+            List<String> expectedCardPlaceholders = List.of(
+                    "Номер карты",
+                    "Срок действия",
+                    "CVC",
+                    "Имя и фамилия на карте"
+            );
+            List<String> actualCardPlaceholders = page.getCardPlaceholders();
+            assertEquals(expectedCardPlaceholders, actualCardPlaceholders);
+        });
+
+        Allure.step("Проверка отображения платежных систем", () -> {
+            List<WebElement> cardBrandsImg = page.getCardBrandsImg();
+            assertFalse(cardBrandsImg.isEmpty());
+
+            for (WebElement image : cardBrandsImg) {
+                assertFalse(image.getAttribute("src").isBlank());
+            }
+        });
 
         driver.quit();
     }
